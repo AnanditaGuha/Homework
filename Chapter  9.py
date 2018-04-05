72a0938e941c08e0caecbd707735cbb8cc11dd4c
AnanditaGuha <31958631+AnanditaGuha@users.noreply.github.com>
2018-04-05 08:44:33 -0400

    Chapter 9

---
+++ Chapter  9.py
@@ -0,0 +1,11 @@
+def tupple_test (thistuple):
+    return thistuple
+
+
+mytupple = ("Shell", "Frank")
+print(tupple_test(mytupple))
+#yes you can but be careful with the parens
+
+#2 a tuple is a genralization of a pair
+
+#3 a pair is a kind of tuple
