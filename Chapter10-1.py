cc3211b00ea329ef9de85eed4c8ae026f82948ba
AnanditaGuha <31958631+AnanditaGuha@users.noreply.github.com>
2018-04-12 11:08:04 -0400

    Started Chapter10-1

--- Chapter10-1.py
+++ Chapter10-1.py
@@ -18,13 +18,25 @@ def h3():
 
 def h4():
     wn.bye()                        # Close down the turtle window
-
+def red():
+    tess.color("red")
+def blue():
+    tess.color("blue")
+def green():
+    tess.color ("green")
+def plus():
+    size = tess.pensize()
+    if size <= 20 and size >0:
+        tess.pensize(size +1)
 # These lines "wire up" keypresses to the handlers we've defined.
 wn.onkey(h1, "Up")
 wn.onkey(h2, "Left")
 wn.onkey(h3, "Right")
 wn.onkey(h4, "q")
-
+wn.onkey(red, "r")
+wn.onkey(blue, "b")
+wn.onkey(green, "g")
+wn.onkey(plus, "+")
 # Now we need to tell the window to start listening for events,
 # If any of the keys that we're monitoring is pressed, its
 # handler will be called.
